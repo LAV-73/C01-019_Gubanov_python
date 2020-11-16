35f4d8e6cc8a3b9bb855c8061f900e13f9f8181f
gubanovpm <70346831+gubanovpm@users.noreply.github.com>
2020-11-16 08:22:04 -0700

    Update Unique Digits.py

--- Unique Digits.py
+++ Unique Digits.py
@@ -19,3 +19,14 @@ print("Count of unique digits in second list: ", len(B))
 
 print(*C)
 print("Count of unique digits in first and second list: ", len(C))
+
+# Example:
+#1 2 3 4 5 5 4 3 2 1
+#6 7 6 5 4 2 1 9
+#correct answer
+#1 2 3 4 5
+#Count of unique digits in first list:  5
+#1 2 4 5 6 7 9
+#Count of unique digits in second list:  7
+#1 2 3 4 5 6 7 9
+#Count of unique digits in first and second list:  8
